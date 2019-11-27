da3c03039d2c3601ee76a934a1d757e0deeaaed1
flurin-conradin <f.conradin@gmail.com>
2019-11-27 23:36:37 +0100

    templates: own implementation of add Todo

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,39 @@ Template.body.helpers({
         { text: 'This is task 3' },
     ],
 });
+
+//
+//  Own implementation of add Todo
+//
+
+
+Template.body.onCreated(function onCreated() {
+    this.fTasks = new ReactiveVar([
+        { text: 'This is task 7' },
+        { text: 'This is task 8' },
+        { text: 'This is task 9' },
+    ]);
+    this.newTodo = new ReactiveVar("Thelk");
+});
+
+Template.body.helpers({
+    fTasks() {
+        return Template.instance().fTasks.get();
+    },
+    newTodo() {
+        return Template.instance().newTodo.get();
+    }
+});
+
+Template.body.events({
+    'click button'(event, instance) {
+        event.preventDefault();
+        let temp = Template.instance().fTasks.get();
+        temp.push({ text: instance.newTodo.get() });
+        instance.fTasks.set(temp);
+        instance.newTodo.set("");
+    },
+    'keyup input'(event, instance) {
+        instance.newTodo.set(event.target.value)
+    }
+});
