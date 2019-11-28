da3ceb9c4b923b013687542761bac45726ebfa08
flurin-conradin <f.conradin@gmail.com>
2019-11-28 17:04:42 +0100

    Temporary UI states: refactor, align with tutorial

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,20 +5,22 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 import { Template } from 'meteor/templating';
 
-Template.body.onCreated(function onCreated() {
-    this.stateStore = new ReactiveDict({ 'hideCompleted': false });
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
 });
 
 
 Template.body.helpers({
     tasks() {
-        if (Template.instance().stateStore.get('hideCompleted')) {
-            return Tasks.find({checked: {$ne: true}}, { sort: { createdAt: -1 } });
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
         }
         return Tasks.find({}, { sort: { createdAt: -1 } });
+
     },
     hideCompleted() {
-        return Template.instance().stateStore.get('hideCompleted');
+        return Template.instance().store.get('hideCompleted');
     }
 });
 
@@ -41,7 +43,7 @@ Template.body.events({
         // Clear form
         target.text.value = '';
     },
-    'change .hide-completed'(event, instance) {
-        let stateStore = instance.stateStore.set('hideCompleted',  event.target.checked);
-    }
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 });
