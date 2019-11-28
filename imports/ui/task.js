f0be27d8c113a44227c5689985a2d3fa43e9c25b
flurin-conradin <f.conradin@gmail.com>
2019-11-28 15:12:50 +0100

    Update and remove: refactor to align with tutorial

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -4,17 +4,11 @@ import { Tasks } from '../api/tasks.js';
 import { Template } from 'meteor/templating';
 
 Template.task.events({
-    'click .removeTodo'(event) {
+    'click .delete'(event) {
         event.preventDefault();
-        let id = event.target.id;
-        console.log(id);
-        console.log("Delete stuff", id);
         Tasks.remove(this._id);
     },
-    'click .checkbox'(event) {
-        console.log("clicked checkbox");
-        let checked = !event.target.checked;
-        let toggleChecked = !checked
-        Tasks.update(this._id, {$set: {"checked": toggleChecked}})
+    'click .toggle-checked'(event) {
+        Tasks.update(this._id, {$set: {"checked": !this.checked}})
     }
 });
