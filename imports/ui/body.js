43f994d21d2b662c97fa8774a0a9c05ea334360c
flurin-conradin <f.conradin@gmail.com>
2019-11-28 12:07:24 +0100

    Forms and events: own implementation delete Todo

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,25 +6,35 @@ import { Template } from 'meteor/templating';
 Template.body.helpers({
     tasks() {
         return Tasks.find({}, { sort: { createdAt: -1 } });
-      },
+    },
 });
 
 Template.body.events({
     'submit .new-task'(event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-   
-      // Get value from form element
-      const target = event.target;
-      const text = target.text.value;
-   
-      // Insert a task into the collection
-      Tasks.insert({
-        text,
-        createdAt: new Date(), // current time
-      });
-   
-      // Clear form
-      target.text.value = '';
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+});
+
+Template.task.events({
+    'click .removeTodo'(event) {
+        event.preventDefault();
+        let id = event.target.id;
+        console.log(id);
+        console.log("Delete stuff", id);
+        Tasks.remove(this._id);
     },
 });
