3e43092a187f3078514366e0def4260a0ad9090b
flurin-conradin <f.conradin@gmail.com>
2019-11-28 16:30:26 +0100

    Temporary UI state: ~ own hide-completed

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,25 @@
 import './body.html';
 import './task.js';
 
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 import { Template } from 'meteor/templating';
 
+Template.body.onCreated(function onCreated() {
+    this.stateStore = new ReactiveDict({ 'hideCompleted': false });
+});
+
+
 Template.body.helpers({
     tasks() {
+        if (Template.instance().stateStore.get('hideCompleted')) {
+            return Tasks.find({checked: {$ne: true}}, { sort: { createdAt: -1 } });
+        }
         return Tasks.find({}, { sort: { createdAt: -1 } });
     },
+    hideCompleted() {
+        return Template.instance().stateStore.get('hideCompleted');
+    }
 });
 
 Template.body.events({
@@ -29,4 +41,7 @@ Template.body.events({
         // Clear form
         target.text.value = '';
     },
+    'change .hide-completed'(event, instance) {
+        let stateStore = instance.stateStore.set('hideCompleted',  event.target.checked);
+    }
 });
